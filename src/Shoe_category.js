a6be869a2ec436336f115e9f1cb72d569fc5b16b
abhishekyadav798 <99576491+abhishekyadav798@users.noreply.github.com>
2022-03-13 16:48:38 +0530

    added component

---
+++ src/Shoe_category.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import {Row,Col} from 'reactstrap'
+import './Shoe_category.css'
+
+function Shoe_category() {
+  return (
+    <div id='shoe-category'>
+        <Row>
+            <Col>
+            <h5>OUR CLOTHING AND SHOE CATEGORIES</h5>
+            </Col>
+        </Row>
+        <Row>
+            <Col>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Running Shoes</a><br/>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Running Shorts</a><br/>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Running Boots</a>
+            </Col>
+            <Col>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Running Sneakers</a><br/>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Men Running Shoes</a><br/>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Women Running Shoes</a>
+
+            </Col>
+            <Col>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Men Running Shorts</a><br/>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Girls Running Shoes</a><br/>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Women Running Shorts</a>
+
+            </Col>
+            <Col>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Men Running tshirts</a><br/>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Men Running Sneakers</a><br/>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Women Running tshirt</a>
+
+            </Col>
+            <Col>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Running Shoes</a><br/>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}>Kids Running Shoes</a><br/>
+            <a href='#' style={{textDecoration:"none",color:"rgb(170, 170, 170)"}}> Boys Running Shoes</a>
+
+            </Col>
+            
+        </Row>
+
+    </div>
+  )
+}
+
+export default Shoe_category
