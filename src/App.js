1a8747337c2479debf09a532bc5092a8960d2d56
abhishekyadav798 <99576491+abhishekyadav798@users.noreply.github.com>
2022-03-14 20:49:39 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import './App.css';
-import Div_hide from './Div_hide';
-import MouseHover from './MouseHover';
-import Adidas_shoe_component from './Adidas_shoe_component';
+
 import Adidas_new_component from './Adidas_new_component';
-import Text_overlay from './Text_overlay';
-import Hover_div_hide from './Hover_div_hide';
+
 import Topbar from './Topbar';
 import Navbar1 from './Navbar1';
 import Component_1 from './Component_1';
@@ -12,10 +9,7 @@ import Bottom_text from './Bottom_text';
 import Shoe_category from './Shoe_category';
 import Footer_1 from './Footer_1';
 import Bottom_bar from './Bottom_bar';
-import Component_shoe_1 from './Component_shoe_1';
-import Carousel_try from './Carousel_try';
-import Carousel_try_2 from './Carousel_try_2';
-import Carousel_try_3 from './Carousel_try_3';
+
 import Component_2 from './Component_2';
 
 function App() {
@@ -28,18 +22,12 @@ function App() {
       
       <Adidas_new_component/>
      <Adidas_new_component/>
-     {/* <Text_overlay/> */}
-
-     {/* <Hover_div_hide/> */}
+   
      <Bottom_text/>
      <Shoe_category/>
      <Footer_1/>
      <Bottom_bar/> 
-    {/* <Component_shoe_1/> */}
-    {/* <Carousel_try  /> */}
-    {/* <Carousel_try_2/> */}
-    {/* <Carousel_try_3/> */}
-
+   
     </div>
   );
 }
